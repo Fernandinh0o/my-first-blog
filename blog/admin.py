f712b81f137b97e806f30431865bc00db3a95bdb
Luis <luifernando17@gmail.com>
2024-07-22 14:05:44 -0600

    Templates modificados para mostrar post desde base de datos.

--- blog/admin.py
+++ blog/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
-from .models import Post
+from .models import Post, Product  # Asegúrate de que Product esté definido en models.py
 
 admin.site.register(Post)
+admin.site.register(Product)
--- blog/models.py
+++ blog/models.py
@@ -2,15 +2,12 @@ from django.conf import settings
 from django.db import models
 from django.utils import timezone
 
-
 class Post(models.Model):
     author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     title = models.CharField(max_length=200)
     text = models.TextField()
-    created_date = models.DateTimeField(
-            default=timezone.now)
-    published_date = models.DateTimeField(
-            blank=True, null=True)
+    created_date = models.DateTimeField(default=timezone.now)
+    published_date = models.DateTimeField(blank=True, null=True)
 
     def publish(self):
         self.published_date = timezone.now()
@@ -18,3 +15,12 @@ class Post(models.Model):
 
     def __str__(self):
         return self.title
+
+class Product(models.Model):
+    name = models.CharField(max_length=200)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    created_date = models.DateTimeField(default=timezone.now)
+
+    def __str__(self):
+        return self.name
